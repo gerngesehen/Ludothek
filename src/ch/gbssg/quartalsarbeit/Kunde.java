257de94c67775a5266ee2fe192ea72b013165c27
gerngesehen <38924344+gerngesehen@users.noreply.github.com>
2018-06-17 09:00:14 +0200

    Aufsetzen von Klasse Kunde und Test
    Aufsetzen von Klasse Kunde und Test

---
+++ src/ch/gbssg/quartalsarbeit/Kunde.java
@@ -0,0 +1,37 @@
+package ch.gbssg.quartalsarbeit;
+
+public class Kunde {
+	
+
+	
+	private int ID;
+	private String Vorname;
+	private String Nachname;
+	private date Geburtsdatum;
+	private String Adresse;
+	private String Telefonnummer;
+	private String Email;
+	private Boolean Geschlecht;
+	private Boolean Mitglied;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void reservieren(Spiel spiel)
+	{
+		
+	}
+	
+	public void Ausleihen(Spiel spiel)
+	{
+		
+	}
+	
+	public void zurueckgeben(Spiel spiel)
+	{
+		
+	}
+	
+}
--- src/ch/gbssg/quartalsarbeit/Test.java
+++
@@ -1,7 +0,0 @@
-package ch.gbssg.quartalsarbeit;
-
-public class Test {
-	private int Testvar = 1;
-	private int KleinesTest = 100;
-	private Boolean MussKacken = false;
-}
